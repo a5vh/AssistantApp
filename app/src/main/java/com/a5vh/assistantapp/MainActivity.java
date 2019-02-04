8c6f572325385e01a948dd4139c8acda323554d9
August H. Hummert V <august.hummert5@gmail.com>
2019-02-04 13:27:14 -0600

    2/4
    Actually got themes to work lol

--- app/src/main/java/com/a5vh/assistantapp/MainActivity.java
+++ app/src/main/java/com/a5vh/assistantapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.a5vh.assistantapp;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
